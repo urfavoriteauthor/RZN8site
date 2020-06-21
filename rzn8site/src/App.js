d31866f96d767454a2c718611036ab8933c690c6
Nygel Porterfield <nygel.porterfield@gmail.com>
2020-06-21 01:56:08 -0400

    first

---
+++ rzn8site/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import './assets/css/resonate.css'
+
+function App() {
+  return (
+    <div className="row">
+       
+        <div className='col' id='publishing'>
+          <a href='#'><h1>Publishing Services</h1></a>
+        </div>
+        <div className='col' id='print'>
+        <a href='#'><h1>Printing Services</h1></a>
+        </div>
+      {/* <header className="App-header">
+        <h1>RZN8 Print and Publishing</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          Learn React
+        </a>
+      
+      
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
